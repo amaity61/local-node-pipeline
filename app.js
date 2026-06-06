405cad9d6f8e8f5a856dd457febbbba66d3b94a5
Arindam Maity <amaity61@gmail.com>
2026-06-06 21:23:33 +0530

    check auto deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World! Deployed locally via Jenkins and Docker Arindam.</h1>');
+    res.send('<h1>Hello World! Deployed locally via Jenkins and Docker Auto.</h1>');
 });
 
 app.listen(PORT, () => {
